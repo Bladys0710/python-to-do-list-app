44ee17dedbb1298a003dd686ce15ab2825f0985a
Joleyy <djoleife@gmail.com>
2025-02-17 08:28:58 +0800

    Please help me to figure out the error after the break function. Tho I'm still looking on different articles and online sources, your suggestions would be greatly appreciated

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
 tasks = []
+#prios = []
 
 def addT():
     task= input ("Please enter a task: ")
-    tasks.append(task)
-    print(f"The task {task} added to the list.")
+    while True:
+        try:
+            prio = int(input("Please select the Priority level: 1 = High, 2 = Medium, 3 = Low: "))
+
+            if prio not in [1, 2, 3]:
+                print("Invalid priority level. Please enter 1, 2, 3")
+                continue
+            break
+
+    tasks.append({f"task: {task}, priority: {prio}"})
+    print(f"The task {task} with {prio} level added to the list.")
 
 def listtasks():
     if not tasks:
+
         print ("There are no tasks in here.")
     else:
         print("Current tasks:")
         for index, task in enumerate(tasks):
             print(f"Task # {index}, {task}")
+
+### view by priority and deadline
+
+def priority_order():
+        prio_order = {"High": 1, "Medium": 2, "Low": 3}
+        sorted_order = sorted()
+print(priority_order())
 
 def deleteTask ():
     listtasks()
